ae49f1e88129df0c0413163141c800e5964dc1e3
1 <a1@MacBookPro.local>
2017-05-22 00:47:14 +0300

    JumpAndParallax

--- UnityProject/Assets/HeroRabbit.cs
+++ UnityProject/Assets/HeroRabbit.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class HeroRabbit : MonoBehaviour {
 	public float speed = 1;
-
+	float diff = Time.deltaTime;
+	bool isGrounded = false;
+	bool JumpActive = false;
+	float JumpTime = 0f;
+	public float MaxJumpTime = 2f;
+	public float JumpSpeed = 2f;
 	Rigidbody2D myBody = null;
 	// Use this for initialization
 	void Start () {
@@ -30,12 +35,51 @@ public class HeroRabbit : MonoBehaviour {
 		} else {
 			animator.SetBool ("run", false);
 		}
+		if(this.isGrounded) {
+			animator.SetBool ("jump", false);
+		} else {
+			animator.SetBool ("jump", true);
+		}
+		   
+
+
 		SpriteRenderer sr = GetComponent<SpriteRenderer>();
 		if(value < 0) {
 			sr.flipX = true;
 		} else if(value > 0) {
 			sr.flipX = false;
 		}
+		//class HeroRabit, void FixedUpdate()
+		Vector3 from = transform.position + Vector3.up * 0.3f;
+		Vector3 to = transform.position + Vector3.down * 0.1f;
+		int layer_id = 1 << LayerMask.NameToLayer ("Ground");
+		//Перевіряємо чи проходить лінія через Collider з шаром Ground
+		RaycastHit2D hit = Physics2D.Linecast(from, to, layer_id);
+		if(hit) {
+			isGrounded = true;
+		} else {
+			isGrounded = false;
+		}
+		//Намалювати лінію (для розробника)
+		Debug.DrawLine (from, to, Color.red);
+		if(Input.GetButtonDown("Jump") && isGrounded) {
+			this.JumpActive = true;
+		}
+		if(this.JumpActive) {
+			//Якщо кнопку ще тримають
+			if(Input.GetButton("Jump")) {
+				this.JumpTime += Time.deltaTime;
+				if (this.JumpTime < this.MaxJumpTime) {
+					Vector2 vel = myBody.velocity;
+					vel.y = JumpSpeed * (1.0f - JumpTime / MaxJumpTime);
+					myBody.velocity = vel;
+				}
+			} else {
+				this.JumpActive = false;
+				this.JumpTime = 0;
+			}
+		}
+
 
 	}
 }
