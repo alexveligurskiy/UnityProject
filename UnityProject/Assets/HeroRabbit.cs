a0502f9d6519e44c5d3935542ed21ba683ba90df
1 <a1@MacBookPro.local>
2017-05-29 13:42:22 +0300

    MovingPlatform

--- UnityProject/Assets/HeroRabbit.cs
+++ UnityProject/Assets/HeroRabbit.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class HeroRabbit : MonoBehaviour {
 	public float speed = 1;
+	public float JumpTime = 0f;
+	public float MaxJumpTime = 2f;
+	public float JumpSpeed = 2f;
+
+
 	//float diff = Time.deltaTime;
 	bool isGrounded = false;
 	bool JumpActive = false;
-	float JumpTime = 0f;
-	public float MaxJumpTime = 2f;
-	public float JumpSpeed = 2f;
+
+	Transform heroParent = null;
 	Rigidbody2D myBody = null;
 	// Use this for initialization
 	void Start () {
 		myBody = this.GetComponent<Rigidbody2D> ();
 		LevelControll.current.setStartPosition (transform.position);
+		this.heroParent = this.transform.parent;
 	}
 	
 	// Update is called once per frame
@@ -57,29 +62,41 @@ public class HeroRabbit : MonoBehaviour {
 		RaycastHit2D hit = Physics2D.Linecast(from, to, layer_id);
 		if(hit) {
 			isGrounded = true;
+			if (hit.transform != null
+				&& hit.transform.GetComponent<MovingPlatform>() != null)
+			{
+				//Приліпаємо до платформи
+				this.transform.parent = hit.transform;
+			}
 		} else {
 			isGrounded = false;
+			this.transform.parent = this.heroParent;
 		}
 		//Намалювати лінію (для розробника)
 		Debug.DrawLine (from, to, Color.red);
 		if(Input.GetButtonDown("Jump") && isGrounded) {
 			this.JumpActive = true;
 		}
-		if(this.JumpActive) {
+		if (this.JumpActive){
 			//Якщо кнопку ще тримають
-			if(Input.GetButton("Jump")) {
+			if (Input.GetButton("Jump")){
 				this.JumpTime += Time.deltaTime;
-				if (this.JumpTime < this.MaxJumpTime) {
+				if (this.JumpTime < this.MaxJumpTime)
+				{
 					Vector2 vel = myBody.velocity;
 					vel.y = JumpSpeed * (1.0f - JumpTime / MaxJumpTime);
 					myBody.velocity = vel;
 				}
-			} else {
+			}else{
 				this.JumpActive = false;
 				this.JumpTime = 0;
 			}
 		}
-
-
+		if (this.isGrounded){
+			animator.SetBool("jump", false);
+		}
+		else{
+			animator.SetBool("jump", true);
+		}
 	}
 }
--- UnityProject/Assets/MovingPlatform.cs
+++ UnityProject/Assets/MovingPlatform.cs
@@ -4,36 +4,64 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour {
 	public Vector3 MoveBy;
-	public float Speed = 2f;
-	public float WaitTime = 0.5f;
-
-	public Vector3 pointA;
-	public Vector3 pointB;
+	public float waitTime = 2.0f;
+	public float speed = 0.2f;
+	float time_to_wait;
+	Vector3 pointA;
+	Vector3 pointB;
+	bool going_to_a;
 	// Use this for initialization
 	void Start () {
 		this.pointA = this.transform.position;
-		this.pointB = this.pointA + this.pointB;
+		this.pointB = this.pointA + MoveBy;
+		this.going_to_a = false;
+		this.time_to_wait = -2.0f;
+
 	}
-	bool isArrived(Vector3 pos, Vector3 target){
+
+	bool isArrived(Vector3 pos, Vector3 target)
+	{
 		pos.z = 0;
 		target.z = 0;
-		return Vector3.Distance (pos, target) < 0.02f;
+		return Vector3.Distance(pos, target) < 0.2f;
 	}
 
 	// Update is called once per frame
-	void Update () {
-//		Vector3 target;
-//
-////		if (is_moving_A) {
-//			target = this.pointA;
-//		} else {
-//			target = this.pointB;
-//		}	
-//		Vector3 my_pos = this.transform.position;
-//		if(isArrived(target, my_pos)){
-////			is_moving_A = !is_moving_A;
-//		} else {
-//			Vector3 destination = target - my_pos;
-//		}
+	void FixedUpdate () {
+		time_to_wait -= Time.deltaTime;
+		if (time_to_wait <= 0)
+		{
+
+			Vector3 my_pos = this.transform.position;
+			Vector3 target;
+			if (going_to_a)
+			{
+				target = this.pointA;
+
+			}
+			else
+			{
+				target = this.pointB;
+
+			}
+			if (isArrived(my_pos, target))
+			{
+				going_to_a = !going_to_a;
+				time_to_wait = waitTime;
+			}
+			else
+			{
+				Vector3 destination = target - my_pos;
+				destination.z = 0;
+				float magnitude = destination.magnitude;
+				if (magnitude <= speed || magnitude == 0f)
+				{
+					my_pos = target;
+				}
+				else my_pos += destination / magnitude * speed;
+				this.transform.position = my_pos;
+			}
+		}
+		else { return; }
 	}
 }
