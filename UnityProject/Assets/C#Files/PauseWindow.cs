f7d4f47b28890b718ee9f9b17a4bc569df971ab7
1 <a1@MacBookPro.local>
2017-06-27 03:05:58 +0300

    SomeCorrectionsAndSettingsPopUpAdded

---
+++ UnityProject/Assets/C#Files/PauseWindow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PauseWindow : MonoBehaviour {
+
+	public GameObject Window;
+	public MyButton HomeButton;
+	public MyButton MusicButton;
+	public MyButton CloseButton;
+	public MyButton Background;
+
+	void Start () {
+		Background.signalOnClick.AddListener (this.onClose);
+		HomeButton.signalOnClick.AddListener (this.onPlay);
+		MusicButton.signalOnClick.AddListener (this.onMusic);
+		CloseButton.signalOnClick.AddListener (this.onClose);
+
+
+	}
+
+	void onPlay() {
+		//Do something
+		//SceneManager.LoadScene("StartScene");
+
+	}
+	void onMusic(){
+
+	}
+	void onClose(){
+		Time.timeScale = 1;
+		Destroy(this.gameObject);
+	}
+
+}
--- UnityProject/Assets/C#Files/StartGame.cs
+++ UnityProject/Assets/C#Files/StartGame.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class StartGame : MonoBehaviour {
-
-	public MyButton playButton;
-
+	public GameObject Window;
+	public MyButton StartButton;
+	public MyButton PauseButton;
 	void Start () {
-		playButton.signalOnClick.AddListener (this.onPlay);
+		StartButton.signalOnClick.AddListener (this.onPlay);
+		PauseButton.signalOnClick.AddListener(this.onPause);
 	}
 	void onPlay() {
 		//Do something
 		SceneManager.LoadScene("LevelChooseScene");
+	}
+	void onPause(){
+
+		GameObject obj = GameObject.Find("UI Root").AddChild(this.Window);
+
+		obj.transform.position = this.transform.position;
+		obj.transform.position += new Vector3(-9.0f, 5.0f, 0.0f);
+
+		PauseWindow Window = obj.GetComponent<PauseWindow>();
+
+
 	}
 }
